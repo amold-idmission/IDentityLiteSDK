300a18a846314b4367922df948db6cf4ae9768a2
Amol Deshmukh <amol.deshmukh@idmission.com>
2022-04-17 19:17:02 +0530

    Added SDK v2.0.20.8

---
+++ IDentityLiteSDK.framework/Headers/IDentityLiteSDK.h
@@ -0,0 +1,21 @@
+//
+//  IDentityLiteSDK.h
+//  IDentityLiteSDK
+//
+//  Created by Stefan Kaczmarek on 1/16/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IDentityLiteSDK.
+FOUNDATION_EXPORT double IDentityLiteSDKVersionNumber;
+
+//! Project version string for IDentityLiteSDK.
+FOUNDATION_EXPORT const unsigned char IDentityLiteSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IDentityLiteSDK/PublicHeader.h>
+
+#import "IDentityXSLT.h"
+#import "UIImage+Rotation.h"
+#import "RSA.h"
+#import "FWEncryptorAES.h"
---
+++ IDentityLiteSDK.framework/Headers/IDentityXSLT.h
@@ -0,0 +1,24 @@
+//
+//  IDentityXSLT.h
+//  IDentitySample
+//
+//  Created by Stefan Kaczmarek on 9/6/21.
+//
+
+// derived from https://thedistance.co.uk/ios/xslt-on-ios/
+
+#import <Foundation/Foundation.h>
+
+#import "xsltInternals.h"
+#import "transform.h"
+#import "xsltutils.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IDentityXSLT : NSObject
+
++ (NSString *) transform:(NSString *)xml xsl:(NSString *)xsl;
+
+@end
+
+NS_ASSUME_NONNULL_END
